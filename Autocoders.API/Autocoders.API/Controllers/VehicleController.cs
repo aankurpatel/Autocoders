de773609e8cde989b015ef8769b5d88be7483317
patela <ankur.patel@adp.com>
2015-10-01 14:08:32 -0500

    Cleanup

--- Autocoders.API/Autocoders.API/Controllers/VehicleController.cs
+++ Autocoders.API/Autocoders.API/Controllers/VehicleController.cs
@@ -4,37 +4,17 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using AttributeRouting.Web.Http;
 
 namespace Autocoders.API.Controllers
 {
     [RoutePrefix("api/vehicle")]
     public class VehicleController : ApiController
     {
-        // GET api/<controller>
+         [GET("show")]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<controller>/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<controller>
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        public void Delete(int id)
-        {
         }
     }
 }
--- Autocoders.API/Autocoders.API/Global.asax.cs
+++ Autocoders.API/Autocoders.API/Global.asax.cs
@@ -14,10 +14,7 @@ namespace Autocoders.API
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalConfiguration.Configure(WebApiConfig.Register);         
         }
     }
 }
