5cb15b80783e9eb111989884d8b5427e08b1a4ab
Patel, Ghanshyam <ghanshyam.patel@cdk.com>
2015-10-08 22:26:23 -0500

    removed commented code

--- autocoders.frontend/www/js/addEditVehicle/addEditVehicleCtrl.js
+++ autocoders.frontend/www/js/addEditVehicle/addEditVehicleCtrl.js
@@ -25,18 +25,5 @@
                         alert("Scanning failed: " + error);
                     }
                 );
-
-//            cordova.plugins.barcodeScanner.scan(
-//      function (result) {
-//          alert("We got a barcode\n" +
-//                "Result: " + result.text + "\n" +
-//                "Format: " + result.format + "\n" +
-//                "Cancelled: " + result.cancelled);
-//      },
-//      function (error) {
-//          alert("Scanning failed: " + error);
-//      }
-//   );
-
         };
     });
