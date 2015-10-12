5508b93a80f009521e67cf23de8d529feaab3224
Singhania, Pragya (DS) <Pragya.Singhania@cdk.com>
2015-10-12 09:36:16 -0500

    Added browseVehicles

---
+++ autocoders.frontend/www/js/browseVehicles/browseVehiclesCtrl.js
@@ -0,0 +1,14 @@
+﻿angular.module('starter')
+    .controller('browseVehiclesCtrl', function($scope) {
+    try {
+      window.cordova.plugins.locationServices.geolocation.getCurrentPosition(geolocationSucces);
+    }
+    catch(ex){
+      alert('window.cordova not available');
+    }
+
+    function geolocationSuccess(location){
+      $scope.userLocation = location;
+      alert($scope.userLocation.coords.latitude);
+    }
+    });
