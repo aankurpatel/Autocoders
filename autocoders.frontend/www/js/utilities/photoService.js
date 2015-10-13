1ac73b9af7e0c0620e10085f2456bea6f397ecfd
Singhania, Pragya (DS) <Pragya.Singhania@cdk.com>
2015-10-13 15:00:31 -0500

    Updated photoservice

--- autocoders.frontend/www/js/utilities/photoService.js
+++ autocoders.frontend/www/js/utilities/photoService.js
@@ -2,31 +2,62 @@
  * Created by singhap on 10/12/15.
  */
 angular.module('starter')
-  .factory('photoService', function($q,$cordovaImagePicker,$cordovaCamera) {
+  .factory('photoService', function($q,$cordovaImagePicker,$cordovaCamera,$q) {
+    function getBase64Image(img) {
+      // Create an empty canvas element
+      var canvas = document.createElement("canvas");
+      canvas.width = img.width;
+      canvas.height = img.height;
+
+      // Copy the image contents to the canvas
+      var ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0);
+
+      // Get the data-URL formatted image
+      // Firefox supports PNG and JPEG. You could check img.src to
+      // guess the original format, but be aware the using "image/jpg"
+      // will re-encode the image.
+      var dataURL = canvas.toDataURL("image/png");
+
+      return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+    }
+
 
     return {
-      getPicture: function() {
-        document.addEventListener("deviceready", function () {
+      getPicture: function(){
 
-          var options = {
-            quality: 50,
-            destinationType: Camera.DestinationType.DATA_URL,
-            sourceType: Camera.PictureSourceType.CAMERA,
-            allowEdit: true,
-            encodingType: Camera.EncodingType.JPEG,
-            targetWidth: 100,
-            targetHeight: 100,
-            popoverOptions: CameraPopoverOptions,
-            saveToPhotoAlbum: true,
-            correctOrientation:true
-          };
+      var defer = $q.defer();
+      document.addEventListener("deviceready", function () {
 
-          $cordovaCamera.getPicture(options);
+        var options = {
+          quality: 50,
+          destinationType: Camera.DestinationType.DATA_URL,
+          sourceType: Camera.PictureSourceType.CAMERA,
+          allowEdit: true,
+          encodingType: Camera.EncodingType.JPEG,
+          targetWidth: 100,
+          targetHeight: 100,
+          popoverOptions: CameraPopoverOptions,
+          saveToPhotoAlbum: true,
+          correctOrientation: true
+        };
 
-        }, false);
-      },
+        $cordovaCamera.getPicture(options)
+          .then(function (image) {
+            var base64Image = getBase64Image(image);
+            defer.resolve(base64Image);
+          }, function (err) {
+            console.log(err);
+            defer.reject();
+          });
 
-      uploadImages:function(){
+      }, false);
+      return defer.promise;
+    },
+
+      uploadImages:function () {
+        var defer = $q.defer(),
+          allImages = [];
         var options = {
           maximumImagesCount: 10,
           width: 800,
@@ -34,8 +65,20 @@ angular.module('starter')
           quality: 80
         };
 
-        $cordovaImagePicker.getPictures(options);
+        $cordovaImagePicker.getPictures(options)
+          .then(function (results) {
+            for (var i = 0; i < results.length; i++) {
+              console.log('Image URI: ' + results[i]);
+              var base64Image = getBase64Image(results[i]);
+              allImages.push(base64Image);
+            }
+            defer.resolve(allImages);
+          }, function (err) {
+            defer.reject();
+            console.log(err);
+          });
+        return defer.promise;
 
       }
-    }
+    };
   });
