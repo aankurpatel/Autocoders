5e8cd022a9945c0b3b8a3756e7ecbeb31aafeb2c
Vamshidhar Pothireddy <Vamshidhar.Pothireddy@cdk.com>
2015-10-13 17:05:36 -0500

    quote negotiation updates

--- autocoders.frontend/www/js/quoteNegotiation/quoteCtrl.js
+++ autocoders.frontend/www/js/quoteNegotiation/quoteCtrl.js
@@ -1,5 +1,24 @@
 angular.module('starter')
-.controller('quoteCtrl', function($scope, $stateParams) {
-	$scope.view = {};
-	$scope.vehicle = $stateParams.vehicle;
-});
+.controller('quoteCtrl', [ '$scope','$stateParams','quoteService',
+	function($scope, $stateParams, quoteService) {
+		$scope.vehicle = $stateParams.vehicle;
+		$scope.Error = "";
+		$scope.monthlyPayment = 0;
+		$scope.taxRate = 0;
+		$scope.titlePrice = 0;
+		$scope.registrationFees = 0;
+		//Init
+		(function(){
+			quoteService.getQuote($scope.vehicle.featPrice, $scope.zipCode).then(
+				function(payment){
+					$scope.monthlyPayment = payment.value;
+					$scope.taxRate = payment.taxRate;
+					$scope.titlePrice = payment.titlePrice;
+					$scope.registrationFees = payment.registrationFees;
+				},
+				function(){
+					$scope.Error = "Error fetching quote. Please retry.";
+				}
+			);
+		})();
+	}]);
