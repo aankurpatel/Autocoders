7f03695ebc0246054e9a6447b863bfc67ed8adf2
nasakas <Shiva.Nasaka@adp.com>
2015-10-13 08:02:48 -0500

    maps revert back the changes.

--- autocoders.frontend/www/js/myMaps/locateDealersCtrl.js
+++ autocoders.frontend/www/js/myMaps/locateDealersCtrl.js
@@ -1,28 +1,28 @@
-/**
- * Created by nasakas on 10/12/2015.
- */
-angular.module('starter', ['ngMaps'])
-  .controller('locateDealersCtlr', function($scope) {
-    $scope.map = {
-      center: [40.7, -74]
-    }
-    $scope.marker = {
-      position: [40.7, -74]
-    }
-    //$scope.$on('mapInitialized', function (event, map) {
-    //  map = {
-    //    center: [39, -121],
-    //    options: function () {
-    //      return {
-    //        streetViewControl: false,
-    //        scrollwheel: false
-    //      }
-    //    },
-    //    events: {
-    //      click: function (e, map) {
-    //        alert(e.latLng.lat() + " " + e.latLng.lng());
-    //      }
-    //    }
-    //  };
-    //})
-  });
+///**
+// * Created by nasakas on 10/12/2015.
+// */
+//angular.module('starter', ['ngMaps'])
+//  .controller('locateDealersCtlr', function($scope) {
+//    $scope.map = {
+//      center: [40.7, -74]
+//    }
+//    $scope.marker = {
+//      position: [40.7, -74]
+//    }
+//    //$scope.$on('mapInitialized', function (event, map) {
+//    //  map = {
+//    //    center: [39, -121],
+//    //    options: function () {
+//    //      return {
+//    //        streetViewControl: false,
+//    //        scrollwheel: false
+//    //      }
+//    //    },
+//    //    events: {
+//    //      click: function (e, map) {
+//    //        alert(e.latLng.lat() + " " + e.latLng.lng());
+//    //      }
+//    //    }
+//    //  };
+//    //})
+//  });
