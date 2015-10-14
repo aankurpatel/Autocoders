50ee8c3d59c34d84f31edd46203658831683f834
Patel, Ghanshyam <ghanshyam.patel@cdk.com>
2015-10-14 12:00:15 -0500

    no message

--- autocoders.frontend/www/js/apiProxies/userApiProxy.js
+++ autocoders.frontend/www/js/apiProxies/userApiProxy.js
@@ -1,10 +1,10 @@
 ﻿angular.module('starter')
-    .service('userApiProxy', function ($log, $http, logger) {
+    .service('userApiProxy', function ($log, $http, logger, helper) {
         var self = this;
         var url = 'https://autocoders.azure-mobile.net/tables/userprofile';
 
         self.create = function(user) {
-
+            throw new  Error('not implemented');
         };
 
         self.getUsers = function () {
@@ -13,8 +13,27 @@
         }
 
         self.saveUser = function (user) {
+            logger.log('saving user');
             logger.log(user);
+            user.accountKey = user.accountKey || helper.makeid();
+
             return $http.post(url, user);
+        };
+
+        self.saveDealer = function(dealer) {
+            logger.log('saving dealer');
+        };
+
+        self.getCurrentUser = function() {
+            return JSON.stringify(window.localStorage['userprofile']);
+        };
+
+        self.getDealerProfile = function (accountKey) {
+            logger.log('getting dealer profile');
+            return {
+                accountKey: 'test',
+                adderss1: 'add 1'
+            }
         };
 
         self.getUserTokens = function(accountKey) {
