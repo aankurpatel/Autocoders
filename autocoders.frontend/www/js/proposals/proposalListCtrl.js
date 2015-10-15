d245dff8ad41a9e31caf3d630fa8b66ac679b39b
Singhania, Pragya (DS) <Pragya.Singhania@cdk.com>
2015-10-15 13:28:36 -0500

    updated

--- autocoders.frontend/www/js/proposals/proposalListCtrl.js
+++ autocoders.frontend/www/js/proposals/proposalListCtrl.js
@@ -17,8 +17,8 @@ angular.module('starter')
                     proposal.buyerquote = JSON.parse(value.buyerquote);
                     proposal.sellerquote = JSON.parse(value.sellerquote);
                     proposal.buyer = JSON.parse(value.buyer);
-                    if($scope.customerAccountKey){//show all proposals from this user
-                      if(proposal.buyer.accountKey ===$scope.customerAccountKey) {
+                    if($rootScope.customerAccountKey){//show all proposals from this user
+                      if(proposal.buyer.accountKey ===$rootScope.customerAccountKey) {
                         $scope.proposals.push(proposal);
                       }
                     }else if(proposal.status ==="Negotiating")//only show Negotiating proposals
