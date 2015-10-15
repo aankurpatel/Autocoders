133e3c0674fcab126eb97f1f935c30ab49665514
patela <ankur.patel@adp.com>
2015-10-15 08:54:42 -0500

    add routing to page for notification

--- autocoders.frontend/www/js/app.js
+++ autocoders.frontend/www/js/app.js
@@ -55,19 +55,21 @@ angular.module('starter', ['ionic', 'ionic.service.core', 'starter.controllers',
                 }
                 break;
             case 'message':
-                alert(JSON.stringify(notification));
+               // alert(JSON.stringify(notification));
                 var confirmPopup = $ionicPopup.confirm({
                     title: notification.title,
                     template: notification.message
                 });
                 confirmPopup.then(function(res) {
                     if (res) {
-                        alert('navifgationg to ' + notification.route);
-                        if (notification.route) {
-                            $state.go(notification.route);
+                        var route = notification.payload.route;
+                        var data = notification.payload.data; //quote object from seller or buyer
+                        alert('navigating to ' + route);
+                        if (route) {
+                            $state.go(route);
                         }
                     } else {
-                        console.log('You are not sure');
+                        $state.go('app.browse'); //default
                     }
                 });
                 break;
